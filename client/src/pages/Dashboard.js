1c0b0404af84fbb4e76a707aca10c235f1f6703f
Ammar1201 <aamour1201@gmail.com>
2023-01-04 14:32:34 +0200

    added redux and dashboard

--- client/src/pages/Dashboard.js
+++ client/src/pages/Dashboard.js
@@ -1,8 +1,10 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { verifyTokenRequest } from "../api/Api";
+import classes from './Dashboard.module.css';
 
 const Dashboard = () => {
+  const [operation, setOperation] = useState('');
   const navigate = useNavigate();
 
   useEffect(() => {
@@ -21,10 +23,45 @@ const Dashboard = () => {
     }
   }, [navigate]);
 
+  const clickHandle = ({ target }) => {
+    switch (target.id) {
+      case 'newAppointment':
+        setOperation('newAppointment');
+        break;
+      case 'myAppointments':
+        setOperation('myAppointments');
+        break;
+      case 'updateProfile':
+        setOperation('updateProfile');
+        break;
+      case 'logout':
+        sessionStorage.removeItem('user');
+        navigate('/');
+        break;
+      default:
+        break;
+    }
+  };
+
   return (
-    <>
-      <div>Dashboard</div>
-    </>
+    <div>
+      <div className={classes.header}>
+        <h1>Dash Board</h1>
+      </div>
+      <div className={classes.container}>
+        <div className={classes.sidebar}>
+          <button id="newAppointment" onClick={clickHandle} >New Appointment</button>
+          <button id="myAppointments" onClick={clickHandle}>My Appointments</button>
+          <button id="updateProfile" onClick={clickHandle}>Update Profile</button>
+          <button id="logout" onClick={clickHandle}>Logout</button>
+        </div>
+        <div className={classes.main}>
+          {operation && operation === 'newAppointment' && <h1>New Appointment</h1>}
+          {operation && operation === 'myAppointments' && <h1>My Appointments</h1>}
+          {operation && operation === 'updateProfile' && <h1>Update Profile</h1>}
+        </div>
+      </div>
+    </div>
   )
 }
 
